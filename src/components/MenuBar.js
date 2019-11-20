67b8e47243969a13d4c083268be11a9a1e9e8379
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-11-20 10:00:51 -0500

    donezo

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from 'react';
 
-const MenuBar = (props) => {
-
+class MenuBar extends React.Component {
   /*
 
   The 'a' tags below are the menu items. Think about the way a menu 
@@ -13,26 +12,38 @@ const MenuBar = (props) => {
 
   */
 
-  return (
-    <div className="ui four item menu">
-      <a className="item active" id="profile">
-        <i className="user large icon" id="profile"/>
-      </a>
+  sendDetailsToDisplay = e => {
+    e.stopPropagation();
+    this.props.setDetailsToDisplay(e.target.id);
+    e.target.parentNode.querySelector('.active').className = 'item';
+    e.target.className = 'item active';
+  };
 
-      <a className="item" id="photo">
-        <i className="photo large icon" id="photo"/>
-      </a>
+  render() {
+    return (
+      <div className="ui four item menu">
+        <a
+          onClick={this.sendDetailsToDisplay}
+          className="item active"
+          id="profile"
+        >
+          <i className="user large icon" id="profile" />
+        </a>
 
-      <a className="item" id="cocktail">
-        <i className="cocktail large icon" id="cocktail"/>
-      </a>
+        <a onClick={this.sendDetailsToDisplay} className="item" id="photo">
+          <i className="photo large icon" id="photo" />
+        </a>
 
-      <a className="item" id="pokemon"> 
-        <i className=" themeisle large icon" id="pokemon"/>
-      </a>
-    </div>
-  )
+        <a onClick={this.sendDetailsToDisplay} className="item" id="cocktail">
+          <i className="cocktail large icon" id="cocktail" />
+        </a>
 
+        <a onClick={this.sendDetailsToDisplay} className="item" id="pokemon">
+          <i className=" themeisle large icon" id="pokemon" />
+        </a>
+      </div>
+    );
+  }
 }
 
-export default MenuBar
+export default MenuBar;
